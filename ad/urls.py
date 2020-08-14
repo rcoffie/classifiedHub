4e8ac7625a527f45b050097cd8dc22c314c82b60
Robert Coffie <rcoffie22@yahoo.com>
2020-08-14 15:48:27 +0100

    fridayc2

--- ad/urls.py
+++ ad/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-   path('',views.ads,name='ads')
+   path('',views.ads,name='ads'),
+   path('<int:ad_id>',views.ad,name='ad'),
 ]
--- ad/views.py
+++ ad/views.py
@@ -10,3 +10,12 @@ def ads(request):
   ads = Ads.objects.order_by('-date_posted').filter(published=True)
   context = {'ads':ads,}
   return render(request,'ads/shop.html',context)
+
+
+
+
+def ad(request,ad_id):
+  ad = get_object_or_404(Ads,pk=ad_id)
+  context = {'ad':ad,}
+  return render(request,'ads/ad.html',context)
+  
