2a37c30e0f0fa6409406c981b0725c769f2605c7
Robert Coffie <rcoffie22@yahoo.com>
2020-08-17 16:42:47 +0100

    mondayc

--- account/views.py
+++ account/views.py
@@ -15,4 +15,30 @@ def login(request):
 
 
 def register(request):
+  if request.method == 'POST':
+    first_name = request.POST['first_name']
+    last_name  = request.POST['last_name']
+    username   = request.POST['username']
+    email      = request.POST['email']
+    password   = request.POST['password']
+    password2  = request.POST['password2']
+    if password == password2:
+      if User.objects.filter(username=username).exists():
+        messages.error(request,'username alerady exits')
+        return redirect('register')
+      else:
+        if User.objects.filter(email=email).exists():
+          messages.error(request,'email address already taken')
+          return redirect ('register')
+        else:
+          user = User.objects.create_user(first_name=first_name,last_name=last_name,username=username,email=email,password=password) 
+          user.save() 
+          messages.success(request,'account created successfully you can now login ') 
+          return redirect('login')
+    else:
+      messages.error(request,'password dont match')
+      return redirect('register')
+
+      
+
   return render(request,'account/registration.html')
